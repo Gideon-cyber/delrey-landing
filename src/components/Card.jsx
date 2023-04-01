6162a98fad8bc4c084127ba0683412c1870c8dfc
Gideon Nwokpor <gideonjohnson70@gmail.com>
2023-04-01 17:47:21 +0100

    new commit showing road map section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,24 +2,17 @@ import React from "react";
 
 import Event from "../public/event1.svg";
 
-const Card = ({ title, walletAdd, description }) => {
+const Card = ({ title, description }) => {
   return (
-    <div className="w-full md:w-[343px] overflow-hidden rounded-[18px] flex items-center flex-col shadow-card">
-      <div className="flex-1">
-        <img src={Event} alt="event-pic" />
-      </div>
-      <div className="flex-1 flex items-start gap-4 bg-white py-[20px] px-[30px]">
-        <div className="flex items-start gap-[10px] flex-col">
+    <div className="w-full md:w-[320px] h-auto sm:min-h-[370px] overflow-hidden rounded-[10px] flex items-center flex-col shadow-card">
+      <div className="flex-1 flex items-center justify-center gap-4 bg-[#ad70eb] py-[20px] px-[30px]">
+        <div className="flex items-center justify-center gap-[20px] flex-col">
           <div className="flex items-center justify-between w-full">
-            <span className="text-[14px] leading-[150%] font-b-700 text-black">
+            <span className="text-[18px] leading-[24px] font-b-700 text-white">
               {title}
             </span>
-            <span className="text-[10px] py-2 px-3 bg-pinkPrim text-white">{`${walletAdd.slice(
-              0,
-              6
-            )}...`}</span>
           </div>
-          <span className="text-[#6A6A6A] text-[14px] leading-[21px]">
+          <span className="text-white text-[14px] leading-[21px]">
             {description}
           </span>
         </div>
--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import Card from "./Card";
 
 const Roadmap = () => {
+  const roadmapArr = [
+    {
+      title: "Phase 1: Community building",
+      description:
+        "The first phase of the project will focus on building a strong and supportive community. This will involve organizing regular community events, social media campaigns, and other activities to engage with the community. The team will also work to establish partnerships with other projects and communities to expand the reach of the DelRey Inu community.",
+    },
+    {
+      title: "Phase 2: Listings and Partnerships",
+      description:
+        "In the second phase, the team will work to get the token listed on centralized exchanges and other platforms. This will increase the visibility of the project and help to attract more investors and supporters. Additionally, the team will look to form partnerships with other projects and communities that share the same values as DelRey Inu.",
+    },
+    {
+      title: "Phase 3: Platform Development",
+      description:
+        "In the final phase, the team will work on developing a platform that will provide additional services to the community.This could include a wallet, staking, and yield farming capabilities, providing more opportunities for the community to engage with the token.",
+    },
+  ];
   return (
     <div className="flex items-center justify-center py-[16px] px-[16px] md:py-[20px] md:px-[60px] min-h-screen h-auto w-full max-w-7xl mx-auto">
       <div className="flex items-center flex-col-reverse lg:flex-row py-[2rem] gap-[40px] lg:gap-[126px]">
@@ -9,6 +27,16 @@ const Roadmap = () => {
             <h1 className="text-[36px] lg:text-[58px] lg:leading-[65px] font-bold tracking-[0.2px] w-[80%] lg:w-full text-center">
               ROAD MAP
             </h1>
+          </div>
+          <div className="w-full flex flex-col items-center justify-center">
+            <div className="hidden sm:flex"></div>
+            <div className="flex items-center flex-col sm:flex-row gap-4 w-full">
+              {roadmapArr.map(({ title, description }, index) => (
+                <div key={index}>
+                  <Card title={title} description={description} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
